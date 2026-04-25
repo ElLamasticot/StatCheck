d4181a42e1b3a68738573b439ecb082661f7fe19
MxPerrot <44461157+MxPerrot@users.noreply.github.com>
2026-04-25 22:02:41 +0200

    Link form to cogitative spirits

---
+++ littany_of_calculation.js
@@ -0,0 +1,12 @@
+const params = new URLSearchParams(window.location.search);
+console.table(params)
+
+/*
+ATTAQUANT
+A_A     Nombre d'attaques
+A_WS    Capacité de combat
+A_S     Force
+A_AP    Pénétration d'armure
+A_D     Dégats
+
+*/
